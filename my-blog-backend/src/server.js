fb843b5ccf858a5e9e590e9fbcbfeee11d9353d6
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-11-11 23:57:27 +0000

    testing upvote

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,6 +3,18 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
+let articlesInfo = [{
+  name: 'learn-react',
+  upvotes: 0,
+}, {
+  name: 'learn-node',
+  upvotes: 0,
+}, {
+  name: 'mondodb',
+  upvotes: 0,
+}]
+
+
 // app.post('/', (req, res) => {
 //   res.send(`Hello ${req.body.name}!`);
 // });
@@ -11,6 +23,17 @@ const PORT = 3000;
 //   const name = req.params.name;
 //   res.send(`Hello ${name}!!`);
 // })
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find(a => a.name === name);
+  if (article) {
+  article.upvotes += 1;
+  res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send('That article doesn\'t exist');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
