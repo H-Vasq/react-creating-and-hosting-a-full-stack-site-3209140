3c9af4707a1c1bd7f4bd7f203c8800c7e7e604e5
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-11-11 19:58:36 +0000

    add working article links

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ArticleListPage from './pages/ArticleListPage'
@@ -12,19 +13,22 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>My React Blog</h1>
+        <nav>
+          < NavBar />
+        </nav>
         <div id="page-body">
-          Welcome to my first blog!
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/articles" element={<ArticleListPage />} />
-            <Route path="/articles/:articleId" element={<ArticlePage />} />
-          </Routes>
+          <div id="content-wrapper">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/articles" element={<ArticleListPage />} />
+              <Route path="/articles/:articleId" element={<ArticlePage />} />
+            </Routes>
+          </div>
         </div>
       </div>
-      </BrowserRouter>
-      )
+    </BrowserRouter>
+  )
 }
 
-      export default App
+export default App
--- my-vite-app/src/pages/NotFoundPage.jsx
+++ my-vite-app/src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 const  NotFoundPage = () => {
-    return <h1>This is Not Found Page</h1>
+    return (<h1>This is Not Found Page</h1>);
 };
 
 export default NotFoundPage;
