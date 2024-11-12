9ebddd577b680bf33eca56129a1010768a52a26e
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-11-12 00:16:22 +0000

    add comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -6,32 +6,38 @@ const PORT = 3000;
 let articlesInfo = [{
   name: 'learn-react',
   upvotes: 0,
+  comments: [],
 }, {
   name: 'learn-node',
   upvotes: 0,
+  comments: [],
 }, {
-  name: 'mondodb',
+  name: 'mongodb',
   upvotes: 0,
+  comments: [],
 }]
-
-
-// app.post('/', (req, res) => {
-//   res.send(`Hello ${req.body.name}!`);
-// });
-
-// app.get('/hello/:name', (req, res) => {
-//   const name = req.params.name;
-//   res.send(`Hello ${name}!!`);
-// })
 
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
   const article = articlesInfo.find(a => a.name === name);
   if (article) {
-  article.upvotes += 1;
-  res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
   } else {
     res.send('That article doesn\'t exist');
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find(a => a.name === name);
+  if (article) {
+      article.comments.push({ postedBy, text });
+      res.send(article.comments);
+  } else {
+      res.send('That article doesn\'t exist');
   }
 });
 
